6701f9d1d1f0923e86e0efcadbfa0c6bdd033a18
Diekoloreoluwa Mayowa Akinsete <dj_blueewastaken@outlook.com>
2023-10-11 22:37:34 +0100

    Update 1.0.2
    - Added context menu options for copying the path of the project, opening the project in both the file explorer and terminal

--- Main.cs
+++ Main.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Flow.Launcher.Plugin.Godot
 {
-    public class Godot : IPlugin, ISettingProvider
+    public class Godot : IPlugin, ISettingProvider, IContextMenu
     {
         private const string GodotFileExtension = ".godot";
         private const string GodotIconPath = "Assets\\Godot.png";
@@ -26,6 +27,66 @@ namespace Flow.Launcher.Plugin.Godot
             _settings = context.API.LoadSettingJsonStorage<Settings>();
         }
 
+        public List<Result> LoadContextMenus(Result selectedResult)
+        {
+            List<Result> contextResults = new List<Result>
+            {
+                new () 
+                {
+                    Title = "Open containing folder",
+                    IcoPath = GodotIconPath,
+                    Action = x =>
+                    {
+                        Process explorerProc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "explorer.exe",
+                                Arguments = selectedResult.SubTitle,
+                                CreateNoWindow = true,
+                                UseShellExecute = true
+                            }
+                        };
+                        explorerProc.Start();
+                        return true;
+                    },
+                },
+
+                new ()
+                {
+                    Title = "Copy Path",
+                    IcoPath = GodotIconPath,
+                    Action = x =>
+                    {
+                        Clipboard.SetText(selectedResult.SubTitle);
+                        return true;
+                    },
+                },
+
+                new ()
+                {
+                    Title = "Open with shell",
+                    IcoPath = GodotIconPath,
+                    Action = x =>
+                    {
+                        Process shellProc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "CMD.exe",
+                                WorkingDirectory = selectedResult.SubTitle,
+                                CreateNoWindow = false,
+                                UseShellExecute = true
+                            }
+                        };
+                        shellProc.Start();
+                        return true;
+                    },
+                },
+            };
+            return contextResults;
+        }
+
         public List<Result> Query(Query query)
         {
             List<Result> results = new List<Result>();
@@ -35,7 +96,7 @@ namespace Flow.Launcher.Plugin.Godot
                 var projects = godotProjectsDirectoryInfo.GetDirectories();
                 for (int i = 0; i < projects.Length; i++)
                 {
-                    var singleProject = projects[i].GetFiles();
+                    var singleProject = projects[i].GetFiles(); 
                     singleProject.ToList().ForEach(delegate (FileInfo file)
                     {
                         if (file.FullName.EndsWith(GodotFileExtension))
@@ -43,6 +104,7 @@ namespace Flow.Launcher.Plugin.Godot
                             results.Add(new Result
                             {
                                 Title = file.Directory?.Name ?? "Path not specified",
+                                SubTitle = file.Directory.FullName,
                                 IcoPath = GodotIconPath,
                                 Action = x =>
                                 {
